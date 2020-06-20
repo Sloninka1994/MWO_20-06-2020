f381f1ec24ed7d11120b8baec94394b02671850f
Paweł Tybinka <tybinka@student.agh.edu.pl>
2020-06-20 15:09:21 +0200

    Report 1 - returns tree map

---
+++ src/main/java/pl/edu/agh/mwo/java/Reports/Report1.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.mwo.java.Reports;
+
+import pl.edu.agh.mwo.java.DataModel.RecordEntry;
+import pl.edu.agh.mwo.java.DataModel.RecordFilter;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Report1 {
+    private ArrayList<RecordEntry> recordEntries;
+    public Report1(ArrayList<RecordEntry> recordEntries, int Year){
+        RecordFilter recordFilter = new RecordFilter(recordEntries);
+        this.recordEntries =  recordFilter.byYear(Year);
+    }
+
+    public TreeMap<String, Double> getReport(){
+        TreeMap<String, Double> retVal = new TreeMap();
+        for(int i=0; i < recordEntries.size(); i++){
+            if (retVal.containsKey(recordEntries.get(i).getWorkerName())) {
+                retVal.put(recordEntries.get(i).getWorkerName(), retVal.get(recordEntries.get(i).getWorkerName()) + recordEntries.get(i).getWorkingHours());
+            }else{
+                retVal.put(recordEntries.get(i).getWorkerName(),recordEntries.get(i).getWorkingHours());
+            }
+        }
+        return retVal;
+    }
+
+}
