dc2d917e3bccdf4c24c79bb321b948b7e5f24a28
Paweł Tybinka <tybinka@student.agh.edu.pl>
2020-06-20 12:57:31 +0200

    data model of simple entry - one instance is one row from excel worksheet

---
+++ src/main/java/pl/edu/agh/mwo/java/DataModel/RecordEntry.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.mwo.java.DataModel;
+
+import java.util.Date;
+
+public class RecordEntry {
+    private String workerName;
+    private String projectName;
+    private Date date;
+    private String description;
+    private double workingHours;
+
+    public String getWorkerName() {
+        return workerName;
+    }
+
+    public void setWorkerName(String workerName) {
+        this.workerName = workerName;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(double workingHours) {
+        this.workingHours = workingHours;
+    }
+}
